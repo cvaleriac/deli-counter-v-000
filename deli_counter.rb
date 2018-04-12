3fa6b405576398653b0356530af82b44cce9a130
valeria campilongo <cvaleriac@hotmail.it>
2018-04-12 19:33:30 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,3 +9,7 @@ def line(katz_deli)
       puts current_line
     end
 end
+ def take_a_number(katz_deli, person)
+  katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
