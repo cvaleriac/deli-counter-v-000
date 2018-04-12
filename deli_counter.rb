35e38066cf2d8ddea2d79d814638645f81e35613
valeria campilongo <cvaleriac@hotmail.it>
2018-04-12 18:23:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.size < 1
+    puts "The line is currently empty."
+  else katz_deli.size > 1
+    puts "The line is currently #{katz_deli.size +1}"
+  end
+  def take_a_number(katz_deli, name)
+
+end
