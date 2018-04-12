92d825bddbb3c55088a54ce3e8a28b9170739fd7
valeria campilongo <cvaleriac@hotmail.it>
2018-04-12 19:26:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,13 @@
 def line(katz_deli)
-  if katz_deli.size < 1
+  if katz_deli.size == 0
     puts "The line is currently empty."
-  else katz_deli.size > 1
-    puts "The line is currently #{[katz_deli.size +1]}"
+  else
+      current_line = "The line is currently:"
+      katz_deli.each.with_index(1) do |person, index|
+        current_line << " #{index}. #{person}"
+      end
+      puts current_line
+    end
+  end
   end
 end
