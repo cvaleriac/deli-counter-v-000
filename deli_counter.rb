66b3c99186fbb3b51634d398b5dc6716cbb4a031
valeria campilongo <cvaleriac@hotmail.it>
2018-04-12 18:24:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,6 @@ def line(katz_deli)
   if katz_deli.size < 1
     puts "The line is currently empty."
   else katz_deli.size > 1
-    puts "The line is currently #{katz_deli.size +1}"
+    puts "The line is currently #{[katz_deli.size +1]}"
   end
 end
